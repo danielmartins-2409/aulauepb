08fa83a098873c5c335abd17b9f9c25ad18d7555
danielmartins-2409 <martins.daniel@aluno.uepb.edu.br>
2026-06-18 13:48:21 -0300

    Update ex56_11.py

--- lista3/ex56_11.py
+++ lista3/ex56_11.py
@@ -1,7 +1,11 @@
 n = int(input(""))
 numerate = 1
 soma = 0
-for i in range(1, n+1):
-    soma += i/numerate
-    numerate += 2
-print(soma)
+
+if n <= 0:
+    print("valor inválido")
+else:
+    for i in range(1, n+1):
+        soma += i/numerate    
+        numerate += 2
+    print(soma)
