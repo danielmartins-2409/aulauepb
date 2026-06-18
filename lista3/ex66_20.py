6f62f2fc2bb1dea2bfa00c077c210caf5809e027
danielmartins-2409 <martins.daniel@aluno.uepb.edu.br>
2026-06-18 15:11:55 -0300

    Update ex66_20.py

--- lista3/ex66_20.py
+++ lista3/ex66_20.py
@@ -1,2 +1,27 @@
-divida = float(input("Qual o valor da dívida: "))
+divida = float(input("Qual o valor da dívida?: "))
 
+while divida < 0:
+    print("Valor inválido")
+    divida = float(input("Qual o valor da dívida?: "))
+
+print("Valor da Dívida      % Juros      Parcelas      Valor da Parcela")
+
+
+juros = 0
+parcelas = 1
+valor_divida = divida + (divida * juros / 100)
+valor_parcela = valor_divida / parcelas
+print(f"R$ {valor_divida:.2f}            {juros}           {parcelas}             R$ {valor_parcela:.2f}")
+
+juros = 10
+parcelas = 3
+valor_divida = divida + (divida * juros / 100)
+valor_parcela = valor_divida / parcelas
+print(f"R$ {valor_divida:.2f}            {juros}          {parcelas}             R$ {valor_parcela:.2f}")
+
+
+juros = 15
+parcelas = 6
+valor_divida = divida + (divida * juros / 100)
+valor_parcela = valor_divida / parcelas
+print(f"R$ {valor_divida:.2f}            {juros}          {parcelas}             R$ {valor_parcela:.2f}")
