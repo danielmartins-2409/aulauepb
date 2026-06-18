619b19c41c234528de53ec861b8e29bb7fdd1667
danielmartins-2409 <martins.daniel@aluno.uepb.edu.br>
2026-06-18 11:44:11 -0300

    Atualizar o ex6.py

--- lista1/ex6.py
+++ lista1/ex6.py
@@ -1,6 +1,6 @@
-lado = float(input("Insira o valor do lado do quadrado: "))
+lado = float(input("Insira o valor do lado do quadrado em cm: "))
 
 area = lado ** 2
 dobro_da_area = area * 2
 
-print(f"o dobro da area do quadrado é {dobro_da_area:.3f}")
+print(f"o dobro da area do quadrado é {dobro_da_area:.3f}cm^2")
