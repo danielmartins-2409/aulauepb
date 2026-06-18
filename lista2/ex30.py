90a1b7eaf2a07ce8fd75ed83d88ea8409a8fa0dc
danielmartins-2409 <martins.daniel@aluno.uepb.edu.br>
2026-06-18 12:50:10 -0300

    Update ex30.py

--- lista2/ex30.py
+++ lista2/ex30.py
@@ -5,9 +5,18 @@ valorHora = float(input("Insira o valor ganho por hora: "))
 
 if valorHora > 0:
     horas = float(input("Quantidade de horas trabalhadas: "))
-    if horas > 0:
+    if horas >= 0:
         salarioBruto = valorHora * horas
-        ir = salarioBruto * 0.05
+        if salarioBruto <= 900: 
+            ir_p = 0
+            
+        elif salarioBruto <= 1500:
+            ir_p = 0.05
+        elif salarioBruto <= 2500:
+            ir_p = 0.10
+        else:
+            ir_p = 0.2 
+        ir = salarioBruto *ir_p
         inss = salarioBruto * 0.10
         fgts = salarioBruto * 0.11
         descontos = ir + inss
@@ -16,11 +25,11 @@ if valorHora > 0:
         print(f"""
                   Salário Bruto = R$ {salarioBruto}
 -------------------------------------------------
-                  (-) IR = R$ {ir}
+                  (-) IR({ir_p*100:.0f}%) = R$ {ir}
 -------------------------------------------------
-                  (-) INSS = R$ {inss}
+                  (-) INSS (10%)  = R$ {inss}
 -------------------------------------------------
-                  FGTS = R$ {fgts}
+                  FGTS (11%) = R$ {fgts}
 -------------------------------------------------
                   Total de Descontos = R$ {descontos}
 -------------------------------------------------
