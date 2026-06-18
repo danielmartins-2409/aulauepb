b3b88c4baecf41817f8a76b7eff3acaad208e696
danielmartins-2409 <martins.daniel@aluno.uepb.edu.br>
2026-06-18 13:02:18 -0300

    Update ex33.py

--- lista2/ex33.py
+++ lista2/ex33.py
@@ -1,13 +1,21 @@
 lado1 = float(input("Insira o lado do triangulo: "))
-lado2 = float(input("Insira outro lado do triangulo: "))
-lado3 = float(input("Insira o outro lado do triangulo: "))
-
-if (lado1 + lado2) > lado3 and (lado2 +lado3) > lado1 and (lado1 + lado3) > lado2:
-    if lado1 == lado2 == lado3:
-        print("EQUILATERO")
-    elif lado1 == lado2 or lado2 == lado3 or lado1 == lado3:
-        print("ISOSCELES")
-    else:
-        print("ESCALENO")
+if lado1 <= 0:
+    print("valor inválido")
 else:
-    print("Nao é um triangulo")
+    lado2 = float(input("Insira outro lado do triangulo: "))
+    if lado2 <= 0:
+        print("valor inválido")
+    else:
+        lado3 = float(input("Insira o outro lado do triangulo: "))
+        if lado3 <= 0:
+            print("valor inválido")
+        else:
+            if (lado1 + lado2) > lado3 and (lado2 +lado3) > lado1 and (lado1 + lado3) > lado2:
+                if lado1 == lado2 == lado3:
+                    print("EQUILATERO")
+                elif lado1 == lado2 or lado2 == lado3 or lado1 == lado3:
+                    print("ISOSCELES")
+                else:
+                    print("ESCALENO")
+            else:
+                print("Nao é um triangulo")
