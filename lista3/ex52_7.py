e1607060ad5e4754bbbe26e8b58e6e7d768f0dc4
danielmartins-2409 <martins.daniel@aluno.uepb.edu.br>
2026-06-18 13:42:46 -0300

    Update ex52_7.py

--- lista3/ex52_7.py
+++ lista3/ex52_7.py
@@ -1,5 +1,9 @@
-n = int(input(""))
-h = 0
-for i in range(1, n+1):
-    h += 1/i
-print(h)
+n = int(input("Digite um número: "))
+
+if n <= 0:
+    print("n deve ser maior que zero.")
+else:
+    h = 0
+    for i in range(1, n + 1):
+        h += 1 / i
+    print(h)
