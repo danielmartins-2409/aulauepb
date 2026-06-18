6f661894956de7cfeb06f99dc18cfee04fda740b
danielmartins-2409 <martins.daniel@aluno.uepb.edu.br>
2026-06-18 11:59:28 -0300

    Update ex14.py

--- lista1/ex14.py
+++ lista1/ex14.py
@@ -1,6 +1,6 @@
-distancia = float(input("insira a distancia total percorrida: "))
-combustivelGasto = float(input("Insira o total de combustível gasto: "))
+distancia = float(input("insira a distancia total percorrida em KM: "))
+combustivelGasto = float(input("Insira o total de combustível gasto em litros: "))
 
 consumoMedio = distancia /combustivelGasto
 
-print(f"o consumo medio é {consumoMedio}")
+print(f"o consumo medio é de {consumoMedio} Km/l")
